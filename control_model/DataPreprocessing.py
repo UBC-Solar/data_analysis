31ab94a38affd23dd344eb3d23c0946e99066065
sanar <sanarifa.2011@gmail.com>
2026-03-14 14:36:04 -0700

    retrain model, created python script

--- control_model/DataPreprocessing.py
+++ control_model/DataPreprocessing.py
@@ -5,11 +5,15 @@ from RNN_Dataset import RNN_Dataset
 from sklearn.preprocessing import MinMaxScaler
 from data_tools import query
 import pandas as pd
+import numpy as np
 import os
 import dill
+from data_tools import *
+from localization_roc import *
 
 
-# this file will create a single dataframe to use for the RNN. Further scales the data, creates a testing/training split and makes individual sequences to furhter feed into the RNN.
+#this file will create a single dataframe to use for the RNN. Further scales the data, creates a testing/training split and makes individual sequences to furhter feed into the RNN.
+
 
 def combine_dfs(telemetry_names, index_common, all_dfs):
     combined_df = pd.DataFrame(index=index_common)
@@ -21,73 +25,108 @@ def combine_dfs(telemetry_names, index_common, all_dfs):
     return combined_df
 # get data from sunbeam and influx.
 # use sunbeam instead to save yourself a headache
-def make_df(source, name):
-    dfs = []
 
+from torch.utils.data import DataLoader
+from sklearn.preprocessing import StandardScaler
+#from RNN_Dataset import RNN_Dataset
+#necessary imports
+from sklearn.preprocessing import MinMaxScaler
+from data_tools import query
+import pandas as pd
+import numpy as np
+import os
+import dill
+from data_tools import *
+import control_model.localization_roc
+from control_model.localization_roc import *
+
+
+#this file will create a single dataframe to use for the RNN. Further scales the data, creates a testing/training split and makes individual sequences to furhter feed into the RNN.
+
+
+def combine_dfs(telemetry_names, index_common, all_dfs):
+    combined_df = pd.DataFrame(index=index_common)
+    combined_df.dropna()
+
+    for name, df in zip(telemetry_names, all_dfs):
+        combined_df[name] = df
+
+    return combined_df
+# get data from sunbeam and influx.
+# use sunbeam instead to save yourself a headache
+def make_df(source, event):
+    dfs = []
+    files = []
     client = query.SunbeamClient()
-    for event in ["FSGP_2024_Day_1", "FSGP_2024_Day_2", "FSGP_2024_Day_3"]:
+    for name in ["VehicleVelocity", "MechBrakePressed", "AcceleratorPosition"]:
         file = client.get_file(
             origin="production",
             event=event,
             source=source,
             name=name
-        ).unwrap()
+        ).unwrap().data
+        files.append(file)
 
-        dfs.append(
-            pd.DataFrame(
-                data=file.data,
-                index=file.data.datetime_x_axis
+
+    file_pos = client.get_file(
+            origin="production",
+            event=event,
+            source="localization",
+            name="TrackIndex"
+        ).unwrap().data
+
+    files = TimeSeries.align(files[0], files[1], files[2], file_pos);
+    last_idx = np.where(np.isnan(file_pos))[0][0]
+    file_pos = file_pos[0:last_idx]
+    files.append(file_pos)
+    files = TimeSeries.align(files[0], files[1], files[2], files[3]);
+    for file2 in files:
+            dfs.append(
+                pd.DataFrame(
+                    data=file2,
+                    index=file2.datetime_x_axis
+                )
             )
-        )
+
+
+
 
     return pd.concat(dfs).sort_index()
 
+
 def make_single_df():
-    #mech_brake_pressed, accel_position, speed_kph, position = get_data()
-    out_dir = os.path.join("../../array_temp", "data", "control_state_fsgp_2024")
+   for event in ["FSGP_2024_Day_1", "FSGP_2024_Day_2", "FSGP_2024_Day_3"]:
+       speed_kph, mech_brake_pressed, accel_position, position = make_df(source="ingress", event=event)
 
-    brake_path = os.path.join(out_dir, "brake_pressed.bin")
-    accel_path = os.path.join(out_dir, "acceleration.bin")
-    speed_path = os.path.join(out_dir, "speed_kph.bin")
 
-    filepaths = [brake_path, accel_path, speed_path]
+   pd.merge_asof(speed_kph.sort_index(), mech_brake_pressed)
+   scaler = MinMaxScaler(feature_range=(0, 1))
+  #scale acceleration position before standard scaling.
+   df_accel_position = scaler.fit_transform(accel_position.reshape(-1, 1))
+   radius_of_curvature = calculate_circular_track_curvature(coords(), step=2)
+   calculated_roc = [radius_of_curvature[int(i) - 1] for i in position]
+   calculated_roc_df = pd.DataFrame(calculated_roc).sort_index()
 
-    loaded_datasets = []
-
-    for filepath in filepaths:
-        with open(filepath, "rb") as f:
-            data = dill.load(f)
-            loaded_datasets.append(data)
-
-    # unnpack
-    mech_brake_pressed, accel_position, speed_kph = loaded_datasets
-    df_mech_brake_pressed = pd.DataFrame(mech_brake_pressed, index=mech_brake_pressed.datetime_x_axis)
-   # df_accel_position = pd.DataFrame(accel_position, index=accel_position.datetime_x_axis)
-
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    df_accel_position = scaler.fit_transform(accel_position.reshape(-1, 1))
-   # pos_df = make_df(source="localization", name="TrackIndex")
-    speed_df = make_df(source="ingress", name="VehicleVelocity")
-    all_dfs = [df_mech_brake_pressed, df_accel_position]
-    final_df = pd.merge_asof(
-        df_mech_brake_pressed.sort_index(),
+   all_dfs = [mech_brake_pressed, df_accel_position]
+   final_df = pd.merge_asof(
+        mech_brake_pressed.sort_index(),
         pd.DataFrame(df_accel_position, index = accel_position.datetime_x_axis),
         left_index=True,
         right_index=True,
         direction="nearest"
     )
-    # dfs = pd.concat([pos_df, speed_df], axis=1)
-    final_df = pd.merge_asof(
+   dfs = pd.concat([calculated_roc_df, speed_kph], axis=1)
+   final_df = pd.merge_asof(
         final_df.sort_index(),
-        speed_df.sort_index(),
+        dfs.sort_index(),
         left_index=True,
         right_index=True,
         direction="nearest"
     )
-    final_df.columns = ["brake_pressed", "accel_position", "speed"]
-    final_df = final_df.sort_index()
-    final_df = final_df.ffill().dropna()
-    return final_df
+   final_df.columns = ["brake_pressed", "accel_position", "speed", "ROC"]
+   final_df = final_df.sort_index()
+   final_df = final_df.ffill().dropna()
+   return final_df
 
 
 #given the raw dataframe, creates a testing / training split. Only training data is scaled.
@@ -156,3 +195,5 @@ def make_sequence_datasets(
     )
 
     return train_dataset, test_dataset, train_loader, test_loader, scaler
+
+
