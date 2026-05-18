44db4cfd6a59b7530bee2cc57ae50afe29f0ede3
ricd99 <ryanhuang6391@gmail.com>
2026-05-18 16:10:10 -0700

    fix: acutal working basic query script

--- v4/drag_area_coefficient/scripts/data_query.py
+++ v4/drag_area_coefficient/scripts/data_query.py
@@ -5,19 +5,15 @@ from datetime import datetime
 if __name__ == "__main__":
     client = DBClient()
 
-    start_times = []
-    stop_times = []
+    start = datetime.fromisoformat("2026-04-02T20:00:00Z")
+    stop = datetime.fromisoformat("2026-04-02T20:45:00Z")
 
-    # start = datetime.fromisoformat("2026-04-02T20:00:00Z")
-    # stop = datetime.fromisoformat("2026-04-02T20:45:00Z")
-
-    for start, stop in zip(start_times, stop_times):
-        voltage_data: TimeSeries = client.query_time_series(
-            start=start,
-            stop=stop,
-            field="MotorRotatingSpeed",
-            units="km/h"
-        )
+    data: TimeSeries = client.query_time_series(
+        start=start,
+        stop=stop,
+        field="MotorRotatingSpeed",
+        units="km/h"
+    )
 
     # Plot the data
-    voltage_data.plot()
+    data.plot()
