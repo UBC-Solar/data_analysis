8173aab8ffe1b484bd51c47cc11a3522f1f60f74
ricd99 <ryanhuang6391@gmail.com>
2026-05-18 16:49:49 -0700

    request speed, current, and break data; add current threshold

--- v4/drag_area_coefficient/scripts/data_query.py
+++ v4/drag_area_coefficient/scripts/data_query.py
@@ -2,18 +2,37 @@ from data_tools.collections import TimeSeries
 from data_tools.query import DBClient
 from datetime import datetime
 
+PACK_CURRENT_THRESHOLD = 0.10682  # pack current never reads 0; it settles to this constant (it is rounded up)
+
 if __name__ == "__main__":
     client = DBClient()
 
-    start = datetime.fromisoformat("2026-04-02T20:00:00Z")
-    stop = datetime.fromisoformat("2026-04-02T20:45:00Z")
+    start_times = [datetime.fromisoformat("2026-04-02T20:02:30Z")]
+    stop_times = [datetime.fromisoformat("2026-04-02T20:03:10Z")]
 
-    data: TimeSeries = client.query_time_series(
-        start=start,
-        stop=stop,
-        field="MotorRotatingSpeed",
-        units="km/h"
-    )
+    for start, stop in zip(start_times, stop_times):
+        motor_rotating_speed: TimeSeries = client.query_time_series(
+            start=start,
+            stop=stop,
+            field="MotorRotatingSpeed",
+            units="km/h"
+        )
 
-    # Plot the data
-    data.plot()
+        pack_current: TimeSeries = client.query_time_series(
+            start=start,
+            stop=stop,
+            field="PackCurrent",
+            units="A"
+        )
+
+        brake_pressed: TimeSeries = client.query_time_series(
+            start=start,
+            stop=stop,
+            field="BreakPressed",
+            units=""
+        )
+
+
+
+
+    
