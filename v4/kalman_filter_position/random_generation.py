70688be1fce4914cd036755b069c1f4f51fb46b6
sanar <sanarifa.2011@gmail.com>
2026-07-18 14:22:05 -0700

    set up sensor classes to generate random dataset

---
+++ v4/kalman_filter_position/random_generation.py
@@ -0,0 +1,221 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import radians, cos, sin, asin, sqrt
+
+# the goal here is to create a csv dataset that consists of random data so that we can simulate necessary failure conditions as necessary.
+
+RADIUS_EARTH = 6371000.0
+
+
+# required coordinate utilites.
+
+def latlon_to_xy(lat, lon, lat0, lon0):
+    # this will take in the gps coordinates and return it in east north emtres.
+
+    # using the haversine formula to find the bearing and distance betweeen two GPS points.
+
+    x = RADIUS_EARTH * np.cos(np.deg2rad(lat0)) * np.deg2rad(lon - lon0)
+    y = RADIUS_EARTH * np.deg2rad(lat - lat0)
+    return x, y
+
+
+def xy_to_latlon(x, y, lat0, lon0):
+    lat = lat0 + np.rad2deg(y / RADIUS_EARTH)
+    lon = lon0 + np.rad2deg(x / (RADIUS_EARTH * np.cos(np.deg2rad(lat0))))
+    return lat, lon
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    calculates the great circle distance in km between the two specified points on the eearth
+
+    :param lon1:
+    :param lat1:
+    :param lon2:
+    :param lat2:
+    :return:
+    """
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = (sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2)
+    return RADIUS_EARTH * 2 * asin(sqrt(a))
+
+
+class Path:
+    """
+    reads the csv and interpolates the input data to produce linear lat/lon time series at the required time. the CSV consists of the columns time_s, lat, lon
+
+
+    """
+
+    def __init__(self, csv_path: str):
+        df = pd.read_csv(csv_path)
+        self.t = df["time_s"].values.astype(float)
+        self.lat = df["lat"].values.astype(float)
+        self.lon = df["lon"].values.astype(float)
+
+        self.duration = self.t[-1]
+        self.lat0 = self.lat[0]
+        self.lon0 = self.lon[0]
+
+        # convert to local E-N
+        self.x, self.y = latlon_to_xy(self.lat, self.lon, self.lat0, self.lon0)
+
+    def calculate_distance(self, lat, lon):
+        dist = np.zeros(len(lat))
+
+        for i in np.arange(1, len(lat)):
+            lat1 = lat[i - 1]
+            lon1 = (lon[i - 1])
+            lat2 = (lat[i])
+            lon2 = (lon[i])
+
+            dist[i] = haversine(lon1, lat1, lon2, lat2)
+
+        return (dist)
+
+    def gps_interpolate(self, lat, lon, tstamp):
+        dist = self.calculate_distance(lat, lon)
+        dist_cum_norm = np.cumsum(dist) / np.sum(dist)
+        data = [lat, lon]
+        lat_interp = np.interp(tstamp, self.t, self.lat)
+        lon_interp = np.interp(tstamp, self.t, self.lon)
+        return lat_interp, lon_interp
+
+    def latitude(self, t):
+        # method to create a time series object of the latitude using gps interpolation
+        return np.interp(t, self.t, self.lat)
+
+    def x(self, t):
+        return np.interp(t, self.t, self.x)
+
+    def y(self, t):
+        return np.interp(t, self.t, self.y)
+
+    # velocity in x and y:
+    def vx(self, t):
+        # differentiate the x values and then interpolate to requested time t.
+        return np.interp(t, self.t, np.gradient(self.x, self.t))
+
+    def vy(self, t):
+        return np.interp(t, self.t, np.gradient(self.y, self.t))
+
+    def speed(self, t):
+        return np.hypot(self.vx(t), self.vy(t))
+
+    def ax(self, t):
+        # double derivative of x coordinate.
+        # v = np.gradient(self.vx(t), self.t)
+        v = np.gradient(self.x, self.t)
+        a = np.gradient(v, self.t)
+        return np.interp(t, self.t, a)
+
+    def ay(self, t):
+        v = np.gradient(self.y, self.t)
+        a = np.gradient(v, self.t)
+        return np.interp(t, self.t, a)
+
+
+class Sensor:
+    def __init__(self, rate_hz,
+                 noise_std,
+                 bias,drift_rate,
+                 rng_seed: int = 21):
+        self.rate_hz = rate_hz
+        self.noise_std = noise_std or {}
+        self.bias = bias or {}
+        self.drift_rate = drift_rate or {}
+        self.rng = np.random.default_rng(rng_seed)
+
+    def sample(self, path: Path):
+        """
+        generates measuremesnts over the full path duration.
+        :param path:
+        :return:
+        """
+        t = np.arange(0, path.duration, 1.0 / self.rate_hz)
+        df = self.measure(path, t)
+        df.insert(0, "t", t)
+        self.apply_noise(df)
+        return df
+
+    def measure(self, path, t):
+        """generate measurements for the certain time instance?"""
+        return self.sample(path)[:t]
+
+    def apply_noise(self, df):
+        """applies Gaussian noise, constant bias and time variable drift to sensor data. """
+        t = df["t"].values
+        for col, std in self.noise_std.items():
+            if col in df.columns:
+                df[col] += self.rng.normal(0, std, len(df))  # generate gaussian noise.
+        for col, b in self.bias.items():
+            if col in df.columns:
+                df[col] += b
+        for col, rate in self.drift_rate.items():
+            if col in df.columns:
+                df[col] += rate * t
+
+
+# consider noise in GPS vs IMU:
+class GPSSensor(Sensor):
+    """
+    the GPS noise is mostly high frequency, struggles with precision over accuracy and quick velocity changes.
+    the IMU tracks relative movement i.e. errors can accumulate as we integrate creating a "drift" over time.
+    sensors also might have inherent biases. the imu cannot provide absolute position reference on its own.
+
+
+    - the gps sensor class will output lat, lon at a fixed rate with configurable noise and bias. our current GPS Is at 4Hz and has ~5m noise. in our data, we have also observed weird spikes and a lack of consistency between cellular and telemetry (radio) transfer of data.
+    - drift should consider both spatial and temporal aspects. error is +- (lat, lon) tuple.
+
+    """
+
+    def __init__(self, rate_hz=4.0, noise_m=5.0, bias_m=(0, 0), drift_ms=(0, 0), rng_seed=21):
+        # convert all metre noise to degrees using small angle approximation
+        # use: dely = dellat * pi/180 * r_earth
+
+        super().__init__(rate_hz, rng_seed)
+        noise_lat = noise_m / RADIUS_EARTH * (180 / np.pi)
+        noise_lon = noise_m / RADIUS_EARTH * (180 / np.pi)
+
+        bias_deg_lat = bias_m[1] / RADIUS_EARTH * (180 / np.pi)
+        bias_deg_lon = bias_m[0] / RADIUS_EARTH * (180 / np.pi)
+
+        # could potentially add noise by direclty adding to the x,y coordinates too?
+
+    def measure(self, path, t):
+        return pd.DataFrame({"lat": path.latitude(t), "lon": path.longitude(t)})
+
+
+class IMUSensor(Sensor):
+    def __init__(self, rate_hz=30.0, noise_std_mss=0.05,  # gaussian noise in m/s^2,
+                 bias_mss=0.02, drift_rate=0.0, rng_seed=23):
+        noise_std = {"ax": noise_std_mss, "ay": noise_std_mss, "az": 0.02}
+        bias = {"ax": bias_mss, "ay": bias_mss}
+        drift = {"ax": drift_rate, "ay": drift_rate}
+        super().__init__(rate_hz, noise_std, bias, drift, rng_seed)
+
+    def _measure(self, path: Path, t: np.ndarray) -> pd.DataFrame:
+        return pd.DataFrame({
+            "ax": path.ax(t),
+            "ay": path.ay(t),
+            "az": np.full(len(t), 9.81),
+        })
+
+class SpeedSensor(Sensor):
+    """
+    outputs scalar ground speed in m/s.
+
+    """
+    def __init__(self, rate_hz = 30.0, noise_mps = 0.5, bias_mps = 0.0, slip_factor = 1.0, rng_seed = 44):
+        noise_std = {"speed": noise_mps}
+        bias      = {"speed": bias_mps}
+        super().__init__(rate_hz, noise_std, bias, {}, rng_seed)
+        self.slip_factor = slip_factor
+
+    def measure(self, path: Path, t: np.ndarray) -> pd.DataFrame:
+            return pd.DataFrame({
+                "speed": path.speed(t) * self.slip_factor
+            })
